558ec04e8b26ca156a91afbcffd48255b8528e36
Lyn Nagara <lyn.nagara@gmail.com>
2018-08-09 12:06:45 -0700

    accept optional threshold parameter

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ pub fn main() {
 }
 
 fn get_args() -> (String, String, f32) {
-  let DEFAULT_THRESHOLD = 0.;
+  let default_threshold = 0.;
 
   let args: Vec<_> = args().collect();
+
   let orig = args[1].to_string();
   let new = args[2].to_string();
-  let threshold: f32 = 0.1;
+
+  let threshold = if args.len() >= 4 {
+    (&args[3]).parse::<f32>().unwrap()
+  } else {
+    default_threshold
+  };
 
   (orig, new, threshold)
 }
